042af67b15ec5a30d2dd8e7fefc436c7c3af4dc1
Amit Kumar Bhuiyan <mr.amitkb@gmail.com>
2025-09-08 10:02:40 +0600

    Add to Cart Section Completed

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const showAllPlantsDefault = (plants) => {
                                 <button class="text-[#15803D] bg-[#DCFCE7] rounded-3xl px-4 py-1 font-medium cursor-pointer">${plant.category}</button>
                                 <p class="font-semibold">৳<span>${plant.price}</span></p>
                             </div>
-                            <button class="bg-[#15803D] text-white rounded-3xl w-full px-3 py-2 lg:px-5 lg:py-3 cursor-pointer font-medium">Add to Cart</button>
+                            <button onclick="addToCartBtn(${plant.id})" class="bg-[#15803D] text-white rounded-3xl w-full px-3 py-2 lg:px-5 lg:py-3 cursor-pointer font-medium">Add to Cart</button>
                         </div>
         `
         allPlantsContainer.appendChild(div);
@@ -76,7 +76,7 @@ const showSelectedPlantsByCategory = (plants) => {
                                 <button class="text-[#15803D] bg-[#DCFCE7] rounded-3xl px-4 py-1 font-medium cursor-pointer">${plant.category}</button>
                                 <p class="font-semibold">৳<span>${plant.price}</span></p>
                             </div>
-                            <button class="bg-[#15803D] text-white rounded-3xl w-full px-3 py-2 lg:px-5 lg:py-3 cursor-pointer font-medium">Add to Cart</button>
+                            <button onclick="addToCartBtn(${plant.id})" class="bg-[#15803D] text-white rounded-3xl w-full px-3 py-2 lg:px-5 lg:py-3 cursor-pointer font-medium">Add to Cart</button>
                         </div>
         `
         allPlantsContainer.appendChild(div);
@@ -106,3 +106,55 @@ const showModalForCard = (plant) => {
     `
     document.getElementById('modal_container').showModal();
 }
+
+
+
+// Add to Cart features-------------------------------------------------------------
+const addToCartBtn = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+    const res = await fetch(url);
+    const json = await res.json();
+    showOnCart(json.plants)
+}
+// Show on Cart
+const showOnCart = (plant) => {
+    alert(`${plant.name} has been added to the cart.`);
+    const cartAddedContainer = document.getElementById('cart-added-container');
+    const div = document.createElement('div');
+    div.innerHTML = `
+                <div id="cart-${plant.id}" class="flex items-center justify-between bg-[#F0FDF4] px-3 py-2 rounded-lg">
+                    <div class="space-y-2">
+                        <h3 class="font-semibold">${plant.name}</h3>
+                        <p class="text-[#879395]">৳<span>${plant.price}</span> x 1</p>
+                    </div>  
+                    <button onclick="removeCart(${plant.price}, ${plant.id})" class="fa-solid fa-xmark cursor-pointer"></button>
+                </div>
+    `
+        cartAddedContainer.appendChild(div);
+    
+    // Total Amount
+    const totalAmount = Number(document.getElementById('total-amount').innerText);
+    const plantPrice = plant.price;
+    
+    // Update total amount
+    document.getElementById('total-amount').innerText = totalAmount + plantPrice;
+}
+
+// Remove Cart-----------------------------------------------------------------------
+const removeCart = (price, id) => {
+    // Total Amount
+    const totalAmount = Number(document.getElementById('total-amount').innerText);
+    const removeCartPrice = price;
+    
+    // Remove Tree Amount
+    const reducePrice = totalAmount - removeCartPrice;
+    document.getElementById('total-amount').innerText = reducePrice;
+    
+    // Remove Cart
+    // btn.closest('.cart').remove();
+    // btn.parentElement.remove()
+    document.getElementById(`cart-${id}`).remove();
+}
+
+
+// Loading Spinner--------------------------------------------------------------------
