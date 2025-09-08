cf440ec4926c5a173ceef2a2a0d5210932226ebb
Amit Kumar Bhuiyan <mr.amitkb@gmail.com>
2025-09-08 07:28:33 +0600

    Selected Category Card Show Completed

--- script.js
+++ script.js
@@ -63,5 +63,22 @@ const loadSelectedPlants = async(id) => {
 }
     // Show Plants by categories
 const showSelectedPlantsByCategory = (plants) => {
-    
+    const allPlantsContainer = document.getElementById('all-plants-container');
+    allPlantsContainer.innerHTML = "";
+    plants.forEach(plant => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                        <div class="p-4 bg-white rounded-lg space-y-3 flex items-center justify-between flex-col w-full">
+                            <img class="bg-cover rounded-lg w-full h-52 object-cover" src="${plant.image}" alt="">
+                            <h2 class="font-semibold text-left w-full">${plant.name}</h2>
+                            <p class="text-[#4C545F] text-sm text-justify">${plant.description}</p>
+                            <div class="flex items-center justify-between w-full text-sm md:text-base">
+                                <button class="text-[#15803D] bg-[#DCFCE7] rounded-3xl px-4 py-1 font-medium cursor-pointer">${plant.category}</button>
+                                <p class="font-semibold">৳<span>${plant.price}</span></p>
+                            </div>
+                            <button class="bg-[#15803D] text-white rounded-3xl w-full px-3 py-2 lg:px-5 lg:py-3 cursor-pointer font-medium">Add to Cart</button>
+                        </div>
+        `
+        allPlantsContainer.appendChild(div);
+    })
 }
